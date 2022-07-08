815802d06896f6c983a953589b4d81fa69f7ef23
Luciano Garcia <lualgoritmo@outlook.com>
2022-07-08 18:55:43 -0300

    Ainda com erro

--- app/src/main/java/br/com/zup/desafirickmorth/ui/home/view/HomeActivity.kt
+++ app/src/main/java/br/com/zup/desafirickmorth/ui/home/view/HomeActivity.kt
@@ -3,10 +3,13 @@ package br.com.zup.desafirickmorth.ui.home.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.zup.desafirickmorth.R
+import br.com.zup.desafirickmorth.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/br/com/zup/desafirickmorth/ui/home/viewmodel/personViewModel.kt
@@ -0,0 +1,4 @@
+package br.com.zup.desafirickmorth.ui.home.viewmodel
+
+class personViewModel {
+}
