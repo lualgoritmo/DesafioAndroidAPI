dbd3dd4612837c1af5af37ed639089faff93356d
Luciano Garcia <lualgoritmo@outlook.com>
2022-07-09 00:08:49 -0300

    Iniciando o Adapter

--- app/src/main/java/br/com/zup/desafirickmorth/ui/home/view/HomeAdapter.kt
+++ app/src/main/java/br/com/zup/desafirickmorth/ui/home/view/HomeAdapter.kt
@@ -1,4 +1,25 @@
 package br.com.zup.desafirickmorth.ui.home.view
 
-class HomeAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import br.com.zup.desafirickmorth.databinding.PersonItemBinding
+
+class HomeAdapter() : RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeAdapter.ViewHolder {
+        val binding = PersonItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: HomeAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder(val binding: PersonItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
 }
