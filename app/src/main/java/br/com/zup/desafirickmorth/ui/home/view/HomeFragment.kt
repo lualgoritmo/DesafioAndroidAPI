8449d79006df4cf50bb4be0b0c0ec1f782e67ecb
Luciano Garcia <lualgoritmo@outlook.com>
2022-07-09 15:49:33 -0300

    Adapter Completo, recyclerView completo,falta a consumação de API

--- app/src/main/java/br/com/zup/desafirickmorth/ui/home/view/HomeFragment.kt
+++ app/src/main/java/br/com/zup/desafirickmorth/ui/home/view/HomeFragment.kt
@@ -4,11 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.zup.desafirickmorth.R
+import br.com.zup.desafirickmorth.constants.PERSONAGENS
+import br.com.zup.desafirickmorth.databinding.FragmentHomeBinding
+import br.com.zup.desafirickmorth.domain.Person
 
 class HomeFragment : Fragment() {
-
+    private lateinit var binding: FragmentHomeBinding
+    private var persons = mutableListOf<Person>()
+    private val personAdapter by lazy {
+        HomeAdapter(arrayListOf(), this::clickPersonDetall)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,4 +28,23 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        showRecyclerView()
+    }
+
+    private fun clickPersonDetall(person: Person) {
+        sendDetails()
+    }
+
+    private fun sendDetails() {
+        val bundle = bundleOf(PERSONAGENS to persons)
+        NavHostFragment.findNavController(this)
+            .navigate(R.id.action_homeFragment_to_detallFragment, bundle)
+    }
+
+    private fun showRecyclerView() {
+        binding.rvListPerson.adapter = personAdapter
+        binding.rvListPerson.layoutManager = LinearLayoutManager(context)
+    }
 }
