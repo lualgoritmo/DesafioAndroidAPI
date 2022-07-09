73f41d9f33a90c5d5d917f1d71f4c30c4f7ff5a6
Luciano Garcia <lualgoritmo@outlook.com>
2022-07-09 15:18:21 -0300

    Classe adapter feita, sem testar ainda

--- app/src/main/java/br/com/zup/desafirickmorth/ui/home/view/HomeAdapter.kt
+++ app/src/main/java/br/com/zup/desafirickmorth/ui/home/view/HomeAdapter.kt
@@ -4,22 +4,31 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import br.com.zup.desafirickmorth.databinding.PersonItemBinding
+import br.com.zup.desafirickmorth.domain.Person
 
-class HomeAdapter() : RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
+class HomeAdapter(
+    private var listPerson: MutableList<Person>,
+    private var clickDetallPerson: (person: Person) -> Unit
+) : RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeAdapter.ViewHolder {
         val binding = PersonItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: HomeAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val person = listPerson[position]
+        holder.displayCharacterInformation(person)
+        holder.itemView.setOnClickListener {
+            clickDetallPerson(person)
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = listPerson.size
 
     class ViewHolder(val binding: PersonItemBinding) : RecyclerView.ViewHolder(binding.root) {
-
+        fun displayCharacterInformation(person: Person) {
+            binding.personImage.setImageResource(person.image)
+            binding.ivPersonName.text = person.name
+        }
     }
 }
